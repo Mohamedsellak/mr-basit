93c515f6dcf0bb4fdf6f367a22b976cab08be558
sellak <sellak@sellak.sellak>
2025-01-15 23:44:04 +0100

    Add the ads pop up

--- app/_components/floating-images.tsx
+++ app/_components/floating-images.tsx
@@ -18,10 +18,10 @@ const images = [
   '/1.webp',
   '/2.webp',
   '/3.webp',
-  '/4.webp',
   '/5.webp',
   '/6.webp',
   '/7.webp',
+  '/4.webp',
   '/8.webp',
 ]
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/app/_components/navbar";
-import Image from "next/image";
+import Ads from './_components/ads'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,23 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className={`${inter.className} pb-16 relative`}>
-        {/* Fixed Image */}
-        <div className="fixed left-0 top-0 bottom-0 w-[200px] z-50 pointer-events-none
-          before:absolute before:inset-0 before:bg-gradient-to-r before:from-white/80 before:to-transparent before:z-10">
-          <Image
-            src="/99.png"
-            alt="Decorative Pattern"
-            fill
-            className="object-cover object-left"
-            priority
-            sizes="200px"
-          />
-        </div>
-        {/* Main Content with padding for fixed image */}
-        <div className="pl-[200px]">
+        <div>
           {children}
         </div>
         <Navbar />
+        <Ads />
       </body>
     </html>
   );
